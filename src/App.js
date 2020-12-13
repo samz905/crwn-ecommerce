fbc323e955bf5be6334f21b2fa62d4f1d4b5da74
Samarth Zalte <52416785+samz905@users.noreply.github.com>
2020-12-13 21:31:29 +0530

    Broke homepage into its constituent components and did the styling for the sections on the homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import HomePage from './homepage.component'
+import HomePage from './Pages/homepage/homepage.component'
 import './App.css';
 
 function App() {
---
+++ src/Pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../../Components/directory/directory.component';
+import './homepage.styles.css';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+);
+
+export default HomePage;
