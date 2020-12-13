8e15a4115925d92007887c39439cb6afc102c207
Samarth Zalte <52416785+samz905@users.noreply.github.com>
2020-12-13 23:13:24 +0530

    Added router properties to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 import HomePage from './Pages/homepage/homepage.component'
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hi</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
