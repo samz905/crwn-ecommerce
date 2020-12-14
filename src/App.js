3775cd9b02b99c175f3043394595844ca8658ff3
Samarth Zalte <52416785+samz905@users.noreply.github.com>
2020-12-14 14:19:29 +0530

    Adding preview component and displaying items

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-import HomePage from './Pages/homepage/homepage.component'
+import HomePage from './Pages/homepage/homepage.component';
+import ShopPage from './Pages/shop/shop.component';
 import './App.css';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hi</h1>
-  </div>
-);
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
